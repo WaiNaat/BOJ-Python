9669f66c8b9926c6a1bb511a3af4ae2d2f15212b
qq-kim <qoli0703@gmail.com>
2021-12-12 14:30:29 +0900

    itertools.chain()
    for문을 돌려야 하는 범위가 여러 개일 경우 사용하면 편하다.

---
+++ 1326_v2.py
@@ -0,0 +1,39 @@
+from collections import deque
+from itertools import chain
+
+# functions
+def BFS(start, end, N):
+	if start == end: return 0
+
+	visited = [-1 for _ in range(N)]
+	q = deque([])
+
+	visited[start] = 0
+	q.append(start)
+	while q:
+		'''
+		stone := 현재 개구리 위치
+		step := 개구리가 여태까지 몇 번 이동했는지
+		val := 현재 징검다리에 써 있는 숫자
+		adj := 인접 vertex (이동가능한 징검다리 번호)
+		'''
+		stone = q.popleft()
+		step = visited[stone]
+		val = values[stone]
+		right = range(stone+val, N, val) # 오른쪽으로 이동하는 경우
+		left = range(stone-val, -1, -val) # 왼쪽으로 이동하는 경우
+		for adj in chain(right, left):
+			if visited[adj] == -1:
+				visited[adj] = step + 1
+				if adj == end: return visited[adj]
+				q.append(adj)
+
+	return -1
+
+# input
+N = int(input())
+values = tuple(map(int, input().split()))
+a, b = map(int, input().split())
+
+# process & output
+print(BFS(a - 1, b - 1, N))
